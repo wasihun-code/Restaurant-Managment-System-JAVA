384da103cbc3ca851a00fd10977a108cb388f236
wasihun-code <wasihunageru7947@gmail.com>
2022-11-21 11:54:18 +0530

    CREATE: package commons for common functionalities of the entire application

---
+++ Commmons/Utilities.java
@@ -0,0 +1,23 @@
+package Commmons;
+
+import java.io.IOException;
+
+public class Utilities {
+    // Colors
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+
+    // Method to clear the console screen
+    public static void clearScreen() {
+        try {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
